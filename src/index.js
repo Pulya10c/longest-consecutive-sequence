9c75012d9f73cb6d3584851eb390add8ab06aab4
Pulya10c <sergienya.oleg@yandex.ru>
2018-10-03 12:07:46 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function longestConsecutiveLength(array) {
   // your solution here
+  let step = new Set(array);
+  let count = 1;
+  let max = 1;
+     
+    step.forEach(function(value, value2, set){
+      //alert(value1+" "+value2);
+      if ((step.has(value-1))||step.has(value+1)) {
+        
+      count++;
+          } else {count = 1;}
+        if (count>max) {
+          max = count-1;
+        }
+    });
+return max;
+
 }
